853f599f2d439e4bbc77637184d8aaae6bb6cb7c
ascii-16 <mail.akhil99@gmail.com>
2025-08-06 20:31:07 +0530

    feat: css changes

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-import { router } from './lib/router.js';
+import { router, highlightActiveLink } from './lib/router.js';
 
 function main() {
   window.addEventListener('hashchange', router);
   window.addEventListener('load', router);
+  window.addEventListener('hashchange', highlightActiveLink);
+  window.addEventListener('load', highlightActiveLink);
 }
 
 main();
