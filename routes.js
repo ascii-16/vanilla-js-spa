6ee8e07b19fe3a500a569b959e47250d502a7a14
ascii-16 <mail.akhil99@gmail.com>
2025-08-07 10:51:49 +0530

    feat: listing page

--- routes.js
+++ routes.js
@@ -1,7 +1,9 @@
 import About from "./pages/about/about.js";
 import Home from "./pages/home/home.js";
+import Listing from "./pages/listing/listing.js";
 
 export const routes = {
   home: Home,
   about: About,
+  listing: Listing,
 }
