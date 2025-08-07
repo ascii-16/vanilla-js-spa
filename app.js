a73f849d6f6c793d242d06ec877041025cf3f4ff
ascii-16 <mail.akhil99@gmail.com>
2025-08-07 11:41:40 +0530

    feat: deep routing

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-import { router, highlightActiveLink } from './lib/router.js';
+import { router, navigateTo, highlightActiveLink } from './lib/router.js';
 
 function main() {
-  window.addEventListener('hashchange', router);
-  window.addEventListener('load', router);
   window.addEventListener('hashchange', highlightActiveLink);
   window.addEventListener('load', highlightActiveLink);
+  window.addEventListener('popstate', router);
+  document.addEventListener('DOMContentLoaded', () => {
+    document.body.addEventListener('click', e => {
+      if (e.target.matches('[data-link]')) {
+        e.preventDefault();
+        navigateTo(e.target.href);
+      }
+    });
+
+    router();
+  });
 }
 
 main();
--- routes.js
+++ routes.js
@@ -2,8 +2,8 @@ import About from "./pages/about/about.js";
 import Home from "./pages/home/home.js";
 import Listing from "./pages/listing/listing.js";
 
-export const routes = {
-  home: Home,
-  about: About,
-  listing: Listing,
-}
+export const routes = [
+  { path: '/', name: 'home', component: Home },
+  { path: '/about', name: 'about', component: About },
+  { path: '/listing', name: 'listing', component: Listing }
+]
